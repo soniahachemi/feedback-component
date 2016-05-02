c092d85841ab1f86d5050f1e897fc086f29de853
Sonia Hachemi <sonia.hachemi@corp.ovh.com>
2016-05-02 16:00:11 +0200

    path ws

--- feedback.service.js
+++ feedback.service.js
@@ -1,23 +1,24 @@
 angular.module("feedback.createFeedback")
-.service("wsSubmitFeedback", function ($q, Ws, deviceDetector, $http) {
+    .service("wsSubmitFeedback", function ($q, deviceDetector, $http) {
        this.submitFeedback = function (params) {
             var req = {
-             method: 'POST',
-             url: '/ws/ws.cgi',
-             data: { action: 'submitFeedback',
-                        objectType : 'feedback',
-                        url : params.url,
-                        message : params.message,
-                        screenshot : params.screenshot,
-                        browser : deviceDetector.browser,
-                        device : deviceDetector.device,
-                        os : deviceDetector.os,
-                        os_version : deviceDetector.os_version,
-                        browser_version : deviceDetector.browser_version,
-                        app_name : params.app_name,
-                        app_version : params.app_version,
-                        type : params.type
-                    }
+                 method: 'POST',
+                 url: 'datacenter/tools/feedback/ws/ws.cgi',
+                 data: {
+                     action: 'submitFeedback',
+                     objectType : 'feedback',
+                     url : params.url,
+                     message : params.message,
+                     screenshot : params.screenshot,
+                     browser : deviceDetector.browser,
+                     device : deviceDetector.device,
+                     os : deviceDetector.os,
+                     os_version : deviceDetector.os_version,
+                     browser_version : deviceDetector.browser_version,
+                     app_name : params.app_name,
+                     app_version : params.app_version,
+                     type : params.type
+                }
             }
             return $http(req);
         };
